cc4b68c159f7ab098a5c063e152ee35f9419b5a5
baraahekal <ibaraahekal@gmail.com>
2023-10-07 14:52:00 +0300

    counting the duplicate characters in a string

---
+++ count_duplicates.swift
@@ -0,0 +1,20 @@
+func countDuplicates(_ s:String) -> Int {
+    var string = s.lowercased()
+    var mp: [Character: Int] = [:]
+    
+    for c in string {
+        mp[c, default: 0] += 1
+    }
+    
+    var ct = 0
+    for (_, val) in mp {
+        if val > 1 {
+            ct += 1
+        }
+    }
+    
+    return ct
+}
+
+print(countDuplicates("Indivisibilities")) // 2 'i' and 's'
+
